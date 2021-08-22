142bc757ab17dc3fd61eee969a6d52bd671bed5e
Gonzalo Horacio Emanuel Gonzalez <gonzalo220201@gmail.com>
2021-08-22 19:13:13 -0300

    acabo de hacer un ciclo for

--- js/master.js
+++ js/master.js
@@ -1,3 +1,6 @@
 alert('hola como vamos...');
 let miFamilia = ['Gloria', 'Indira', 'Victor', 'Luis'];
 console.log(miFamilia);
+for (let i = 0; i < miFamilia.length; i++){
+    console.log(miFamilia[i]);
+};
